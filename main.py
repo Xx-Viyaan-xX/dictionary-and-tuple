b34d9cee03f10b3a801f562ecd61d3d78ad701fa
Xx_Viyaan_xX <viyaantheking@gmail.com>
2024-10-29 18:49:07 +0530

    dictionary and tuple

---
+++ main.py
@@ -0,0 +1,21 @@
+dict = {"name" : "viyaan" , "age" : 12}
+
+print(dict)
+
+print(dict.get("name"))
+
+dict["age"]=13
+
+print(dict)
+
+dict["adress"]="Lodha Bellezza"
+
+print(dict)
+
+dict.pop("age")
+
+print(dict)
+
+dict.clear()
+
+print(dict)
---
+++ tuple.py
@@ -0,0 +1,30 @@
+tupl =()
+
+print(tupl)
+
+tupl =(1,2,3,4,5,6,7)
+
+print(tupl)
+
+tupl =(1,"viyaan",2.3,1/2)
+
+print(tupl)
+
+tupl =(1,2,3,[4,5,6],(7,8,9))
+
+print(tupl)
+
+tupl =("v","i","y","a","a","n")
+
+print(tupl[0])
+
+print(tupl[1])
+
+print(tupl[2])
+
+print(tupl[3])
+
+print(tupl[4])
+
+print(tupl[5])
+
